54a7bf9ec8e0d6d2a583fd6c557f1055d66eebcf
a622893 <a622893@MC0QUYJC.ww930.my-it-solutions.net>
2018-11-30 00:28:55 +0530

    Added a Sop concept programs

---
+++ Java_Basic_Programs/src/conceptual_programs/SOP_Question.java
@@ -0,0 +1,24 @@
+package conceptual_programs;
+
+public class SOP_Question {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hellow World-"+3+4+5);   // If first part is string then second part is also treated as string
+		
+		System.out.println(3+4+5+ "-Hellow World");  //If first part is calculation then addition is performed
+		
+		System.out.println(30/5+ "-Hellow World");
+		
+		System.out.println(30%5+ "-Hellow World");
+		
+		System.out.println(30/5*2+ "-Hellow World");  //BODMASS Rule is applied
+		
+		System.out.println("Hellow world-"+ 3+ 4.3f);
+		
+		System.out.println(30+ 40.3f +"-Hellow world");
+
+	}
+
+}
