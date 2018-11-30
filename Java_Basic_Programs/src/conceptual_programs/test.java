106da2a167717a084a439b702fea63dffb6fbfae
yashchouhan <yash.chouhan@atos.net>
2018-11-30 15:43:03 +0530

    Updated Code

--- Java_Basic_Programs/src/conceptual_programs/test.java
+++ Java_Basic_Programs/src/conceptual_programs/test.java
@@ -2,40 +2,26 @@ package conceptual_programs;
 
 public class test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int n;
-		System.out.print("Hello,\nworld");
-		System.out.println("n=" + (-4 + 1/2 + 2*-3 + 5.0));
-		
-		int x = 5;
-		int y = 10;
-		int z = ++x * y--;
-		System.out.println(z);
-		
-		System.out.println("1 + 2 = " + 1 + 2);
-		System.out.println("1 + 2 = " + (1 + 2));
-		
-		int a=2, b=3;
-		if (a==3)
-		if (b==3)
-			System.out.println("InsideIf");
-		else
-			System.out.println("insideelse");
-			System.out.println("yash");
-		System.out.println("****************");
-		
-		int i=10;
-		int j=0;  // you have to initialize a j 
-		if(i==10){
-			j=20;
-		}	
-		else if(i==20){
-			j=30;
-		}
-		System.out.println(j);
+	public static void main(String s[])
+    {
+        int i = 7806;
+        int j = 0760;
 
-		
-	}
+        int total = i + j;
 
+        System.out.println("Total is " + total);
+    }
 }
+
+
+/*
+Output-
+Total is 8302
+
+Explaination-
+Execution of program starts from main. Inside main two integer variables i,j are initialized to 7806, 0760 respectively. Another integer variable total is assigned to sum of variables i, j. 7806 is a decimal number (base = 10) and 0760 is octal (base = 8) as any number preceeded with 0 becomes octal. To add both of them they must be made to same base. So convert 0760 to decimal as follows : 
+7 * (8 ^ 2) + 6 * (8 ^ 1) + 0 * (8 ^ 0) = 496 
+now total = 7806 + 496 = 8302 
+The next statement is a display statement which prints the value of variable total.
+*/
+
