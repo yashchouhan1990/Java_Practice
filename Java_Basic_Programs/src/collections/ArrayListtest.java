ed7c8f9943355efecc6073eec87535e569b31cce
yashchouhan <yash.chouhan@atos.net>
2018-12-18 18:03:40 +0530

    Collection- arraylist practice

---
+++ Java_Basic_Programs/src/collections/ArrayListtest.java
@@ -0,0 +1,56 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayListtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	//	ArrayList arylist= new ArrayList();
+		ArrayList<String> al= new ArrayList<String>();
+		al.add("Yash");
+		al.add("Harsh");
+		al.add("Anil");
+		
+		System.out.println(al);
+/*
+* output- [Yash, Harsh, Anil]
+*/
+		
+	// traverse ArrayList elements using the Iterator interface
+		Iterator itr= al.iterator();
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}
+/*
+ * Output-
+
+Yash
+Harsh
+Anil
+	
+ */
+		//Traversing list through for-each loop
+		System.out.println("Traversing list through for-each loop");
+		for(String str:al)
+		{
+			System.out.println(str);
+		}
+		
+/*
+ * Output-
+Traversing list through for-each loop
+Yash
+Harsh
+Anil
+ * 		
+ */
+		
+		
+		
+	}
+
+}
---
+++ Java_Basic_Programs/src/collections/Student.java
@@ -0,0 +1,17 @@
+package collections;
+
+public class Student {
+
+	String name;
+	int age;
+	int rollno;
+
+	Student(String name, int age, int rollno)
+	{
+		this.name=name;
+		this.age=age;
+		this.rollno=rollno;
+	}
+	
+
+}
