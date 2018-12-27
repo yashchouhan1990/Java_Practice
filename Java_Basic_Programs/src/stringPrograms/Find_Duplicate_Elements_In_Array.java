f31d1fbc04af3cc8f34868c8068e13e78aceb057
yashchouhan <yash.chouhan@atos.net>
2018-12-27 17:46:23 +0530

    String duplicate program

--- Java_Basic_Programs/src/stringPrograms/Find_Duplicate_Elements_In_Array.java
+++ Java_Basic_Programs/src/stringPrograms/Find_Duplicate_Elements_In_Array.java
@@ -1,5 +1,8 @@
 package stringPrograms;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Find_Duplicate_Elements_In_Array {
 
 	public static void main(String[] args) {
@@ -7,14 +10,29 @@ public class Find_Duplicate_Elements_In_Array {
 		
 		String arr[]= {"java", "C", "PHP", "Perl", "C", "java"};
 		
+		//1. compare each element. Time complexity is O(nxn)
 		for (int i=0; i<arr.length; i++)
 		{
-			for(int j=1; j<arr.length-1; j++)
+			for(int j=i+1; j<arr.length; j++)
 			{
-				if (arr[i].equals(arr[j])==false)
+				if (arr[i].equals(arr[j]))
 				System.out.println("Duplicate Element : "+arr[i]);
 			}
 		}
+		
+		System.out.println("***********");
+		
+		//2. using HashSet- It store unique values. Time Complexity is O(n)
+		
+		Set<String> store= new HashSet<String>();
+		for(String name:arr)
+		{
+			if(store.add(name)==false)
+			{
+				System.out.println("Duplicate Element : "+ name);
+			}
+		}
+		
 
 	}
 
